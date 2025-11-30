f7cf873e8cbe4af5172ce273b3ffda0d47697084
Quann <doquan19062004@gmail.com>
2025-11-30 07:07:52 +0000

    update model v4

--- enfr-main/src/main/main.py
+++ enfr-main/src/main/main.py
@@ -34,7 +34,7 @@ args = parser.parse_args()
 # 1.1 Kịch bản thực nghiệm
 experiment_presets = {
     "A1": {"embed_dim": 256, "hidden_dim": 256, "num_layers": 1, "dropout": 0.2,
-           "batch_size": 32, "lr": 0.001, "teacher_forcing_ratio": 0.5, "n_epochs": 1},
+           "batch_size": 32, "lr": 0.001, "teacher_forcing_ratio": 0.5, "n_epochs": 10},
 
     "A2": {"embed_dim": 256, "hidden_dim": 512, "num_layers": 2, "dropout": 0.3,
            "batch_size": 32, "lr": 0.001, "teacher_forcing_ratio": 0.5, "n_epochs": 20},
