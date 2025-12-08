203158c36fce05308139903fe0c10430c4f651b9
Quann <doquan19062004@gmail.com>
2025-12-08 01:32:26 +0000

    setup attention with beemsearch

--- enfr-main/src/main/inference.py
+++ enfr-main/src/main/inference.py
@@ -1,26 +1,30 @@
 import torch
+import torch.nn.functional as F
 
-def translate(sentence, model, src_vocab, trg_vocab, src_tokenizer, max_len=50):
+def translate(sentence, model, src_vocab, trg_vocab, src_tokenizer, max_len=50, method="greedy", beem_sizes=5):
 
     model.eval()
+    device = next(model.parameters()).device
 
     # 1. Tokenize + numericalize
     tokens = [src_vocab.stoi["<sos>"]] \
              + src_vocab.numericalize(src_tokenizer(sentence)) \
              + [src_vocab.stoi["<eos>"]]
 
-    src_tensor = torch.tensor(tokens).unsqueeze(0).to(next(model.parameters()).device)
-    src_len = torch.tensor([len(tokens)]).to(src_tensor.device)
+    src_tensor = torch.tensor(tokens).unsqueeze(0).to(device)
+    src_len = torch.tensor([len(tokens)]).to(device)
 
     # 2. Encoder (đúng 3 giá trị)
     with torch.no_grad():
         encoder_outputs, hidden, cell = model.encoder(src_tensor, src_len)
 
+    use_attention = getattr(model.decoder, "use_attention", False)
+    
     # 3. Decoder init với <sos>
     trg_indexes = [trg_vocab.stoi["<sos>"]]
     input_token = torch.tensor([trg_vocab.stoi["<sos>"]]).to(src_tensor.device)
 
-    use_attention = getattr(model.decoder, "use_attention", False)
+    
 
     for _ in range(max_len):
         with torch.no_grad():
