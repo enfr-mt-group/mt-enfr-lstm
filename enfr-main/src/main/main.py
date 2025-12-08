b69aff267d34144f2f0d913639d059861e335efb
Quann <doquan19062004@gmail.com>
2025-12-08 14:16:37 +0000

    setup attention with beemsearch

--- enfr-main/src/main/main.py
+++ enfr-main/src/main/main.py
@@ -52,19 +52,19 @@ args = parser.parse_args()
 # 1.1 Kịch bản thực nghiệm
 experiment_presets = {
     "A1": {"embed_dim": 256, "hidden_dim": 512, "num_layers": 2, "dropout": 0.3, "batch_size": 32, "lr": 0.001,
-           "teacher_forcing_ratio": 0.5, "n_epochs": 1, "use_attention": False},
+           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": False},
 
     "A2": {"embed_dim": 512, "hidden_dim": 512, "num_layers": 2, "dropout": 0.3, "batch_size": 32, "lr": 0.001,
-           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": True},
+           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": False},
 
     "A3": {"embed_dim": 512, "hidden_dim": 512, "num_layers": 2, "dropout": 0.5, "batch_size": 32, "lr": 0.001,
-           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": True},
+           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": False},
 
     "A4": {"embed_dim": 512, "hidden_dim": 512, "num_layers": 2, "dropout": 0.3, "batch_size": 128, "lr": 0.001,
-           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": True},
+           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": False},
 
     "A5": {"embed_dim": 512, "hidden_dim": 512, "num_layers": 2, "dropout": 0.3, "batch_size": 64, "lr": 0.001,
-           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": True},
+           "teacher_forcing_ratio": 0.5, "n_epochs": 20, "use_attention": False},
 }
 
 # 1.2 áp dụng giá trị preset
